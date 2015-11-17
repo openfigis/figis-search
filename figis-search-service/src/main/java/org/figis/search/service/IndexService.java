94a3570c1a2acb171e88424399fc211f69295e29
erikvaningen <erikvaningen@gmail.com>
2015-11-17 17:47:51 +0100

    indexing one factsheet as the english language as a start.

--- figis-search-service/src/main/java/org/figis/search/service/IndexService.java
+++ figis-search-service/src/main/java/org/figis/search/service/IndexService.java
@@ -1,12 +1,27 @@
 package org.figis.search.service;
 
 import java.io.IOException;
+import java.io.StringWriter;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.apache.solr.common.SolrInputDocument;
+import org.fao.fi.factsheetwebservice.domain.FactsheetDomain;
+import org.fao.fi.factsheetwebservice.domain.FactsheetLanguage;
+import org.fao.fi.factsheetwebservice.domain.LanguageList;
+import org.fao.fi.services.factsheet.client.FactsheetWebServiceClient;
 import org.figis.search.config.ref.FigisSearchException;
+import org.figis.search.xmlsearchenginecontrol.ObjectType;
+import org.figis.search.xmlsearchenginecontrol.XmlSearchEngineControl;
+import org.figis.search.xmlsearchenginecontrol.jaxb.FigisSearchJaxb;
+import org.w3c.dom.Document;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,13 +33,10 @@ public class IndexService {
 	 * domain}/factsheet/{factsheetID}
 	 */
 
-	public String update(String indexName, String domain, String factsheetID) {
+	public String update(String indexName, String domain, String factsheet) {
 		SolrClient client = new HttpSolrClient("http://hqldvfigis2:6101/solr/RefPub");
 
-		SolrInputDocument doc = new SolrInputDocument();
-		doc.addField("full_name_e", "testField");
-		doc.addField("id", "10529");
-		doc.addField("url", "http://firms.fao.org/firms/resource/10529/en");
+		SolrInputDocument doc = composeDoc(domain, factsheet);
 
 		String response = IndexServiceResponse.APPLIED_UPDATE;
 		try {
@@ -35,6 +47,35 @@ public class IndexService {
 			throw new FigisSearchException(e);
 		}
 		return response;
+	}
+
+	private SolrInputDocument composeDoc(String domain, String factsheet) {
+		XmlSearchEngineControl c = new FigisSearchJaxb().unmarshall();
+		ObjectType o = c.getObjectTypeList().stream().filter(w -> w.getName().equals(domain)).findFirst().get();
+		FactsheetDomain d = FactsheetDomain.parseDomain(domain);
+		FactsheetWebServiceClient fc = new FactsheetWebServiceClient("http://www.fao.org/figis/ws/factsheets/");
+		LanguageList ll = fc.retrieveLanguageListInDomain4ThisFactsheet(d, factsheet);
+		SolrInputDocument sd = new SolrInputDocument();
+		for (FactsheetLanguage language : ll.getLanguageList()) {
+			Document doc = fc.retrieveFactsheet(factsheet, d, language);
+			try {
+				DOMSource domSource = new DOMSource(doc);
+				StringWriter writer = new StringWriter();
+				StreamResult result = new StreamResult(writer);
+				TransformerFactory tf = TransformerFactory.newInstance();
+				Transformer transformer = tf.newTransformer();
+				transformer.transform(domSource, result);
+				System.out.println("XML IN String format is: \n" + writer.toString());
+				sd.addField("full_name_e", writer.toString());
+				sd.addField("id", factsheet);
+				sd.addField("url", factsheet);
+
+			} catch (TransformerException e) {
+				log.error(e.getMessage());
+				throw new FigisSearchException(e);
+			}
+		}
+		return sd;
 	}
 
 }
