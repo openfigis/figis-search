5e0a79b1f95071ec5e0ed1585bacca78aa4d07bb
erikvaningen <erikvaningen@gmail.com>
2015-12-02 17:42:06 +0100

    cleaning

--- figis-search-web/src/test/java/org/figis/search/web/resource/IndexResourceTest.java
+++ figis-search-web/src/test/java/org/figis/search/web/resource/IndexResourceTest.java
@@ -75,12 +75,4 @@ public class IndexResourceTest {
 		return s;
 	}
 
-	// @Produces
-	// public FigisSearchServiceConfiguration buildConfiguration() {
-	// FigisSearchServiceConfiguration c = new FigisSearchServiceConfiguration();
-	// c.setFactsheetWebservcieEndPoint("http://figisapps.fao.org/figis/ws/factsheets/");
-	// c.setHttpSolrEndPoint("http://hqldvfigis2:8983/solr/factsheet");
-	// return c;
-	// }
-
 }
